4521b41e982d0432065a382313413aa33b7e5bb6
isabeli allgayer <162700804+allgayer@users.noreply.github.com>
2024-09-12 11:44:02 -0300

    Add files via upload

---
+++ LocalStorage.js
@@ -0,0 +1,10 @@
+function enviar(){
+    var numero = document.getElementById("number").value;
+    
+    localStorage.setItem('numeroUsuario', numero);
+    
+    alert("numero enviado");
+    
+    console.log("numero enviado");
+    
+    }
